7a63757e7f649ba07ec4db6a6cbd855fcc4a6353
Robbie <saucy.cooking@gmail.com>
2019-01-15 17:41:34 -0700

    Commit 3 - index.ejs view rendered at pokemon route

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app     = express();
 
+
 const port = 3000;
 // the array of pokemon
 const Pokemon = require('./models/pokemon.js');
 
-
+app.use(express.static('public'));
 
 app.get('/pokemon', (req,res)=>{
-    res.send(Pokemon);
+    //res.send(Pokemon);
+    res.render('index.ejs', {
+        Pokemon: Pokemon,
+    });
 });
 
 app.listen(port, ()=>{
