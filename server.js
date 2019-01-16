678241ee37ea1e40851a1d80bb7ca4c0bea8c712
Robbie <saucy.cooking@gmail.com>
2019-01-15 18:06:27 -0700

    Commit 5 - added dynamic anchor tags to index.ejs

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app     = express();
-
+const morgan = require('morgan')
 
 const port = 3000;
 // the array of pokemon
 const Pokemon = require('./models/pokemon.js');
 
+// middleware
 app.use(express.static('public'));
+app.use(morgan('short'));
 
+// main pokemon list page at index.ejs
 app.get('/pokemon', (req,res)=>{
     //res.send(Pokemon);
     res.render('index.ejs', {
@@ -15,6 +18,15 @@ app.get('/pokemon', (req,res)=>{
     });
 });
 
+// show each pokemon by ID at show.ejs
+app.get('/pokemon/:id', (req,res)=>{
+    //res.send('You are on the pokemon by id page');
+    res.render('show.ejs', {
+        Pokemon: Pokemon,
+        thisPokemon: Pokemon[req.params.id]
+    })
+});
+
 app.listen(port, ()=>{
     console.log(`server is running on port: ${port}`);
-})
+});
